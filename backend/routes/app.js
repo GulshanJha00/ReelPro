0831de2cf2a6114385d4deea30f1d36392b0e61c
GulshanJha00 <gk4763549@gmail.com>
2025-03-18 14:26:27 +0530

    backend setup

--- backend/routes/app.js
+++ backend/routes/app.js
@@ -3,33 +3,45 @@ const app = express();
 const connect = require("../config/db")
 const User = require("../schema/UserSchema")
 const cookieParser = require("cookie-parser")
+const cors = require("cors")
+const jwt = require("jsonwebtoken")
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}))
 
 app.post("/register",async (req,res)=>{
     
     const {email,password} = await req.body;
 
-    if(!email | !password){
-        res.status(404).json({message: "error"})
+    if(!email || !password){
+        return res.status(404).json({message: "error"})
     }
     await connect();
     try {
         const existingUser = await User.findOne({email});
         if(existingUser){
-            res.status(404).json({message: "User already exist"})
+            return res.status(404).json({message: "User already exist"})
         }
         const schema = await User.create({
             email,password
         })
         schema.save();
-        const jwt = jwt.sign({
+        const token = jwt.sign({
             data: email
           }, 'abc', { expiresIn: '10h' });
-        res.status(200).json({message: "User saved"})
+          res.cookie("token",token, {
+            
+                httpOnly: true, 
+                secure: false,  
+                sameSite: "lax", 
+          })
+        return res.status(200).json({message: "User saved"})
     } catch (error) {
-        res.status(500).json({message: "User not saved"})
+        return res.status(500).json({message: "User not saved"})
     }
 })
 
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,4 @@
 const app = require("./routes/app")
-const cors = require("cors")
  
 app.listen("3001",()=>{
     console.log("Running at port http://localhost:3001")
