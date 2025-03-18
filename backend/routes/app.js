84d9cba226db0973b7315536fb037cdd753501eb
GulshanJha00 <gk4763549@gmail.com>
2025-03-18 14:50:30 +0530

    login added

--- backend/routes/app.js
+++ backend/routes/app.js
@@ -1,48 +1,79 @@
-const express = require("express")
+const express = require("express");
 const app = express();
-const connect = require("../config/db")
-const User = require("../schema/UserSchema")
-const cookieParser = require("cookie-parser")
-const cors = require("cors")
-const jwt = require("jsonwebtoken")
+const connect = require("../config/db");
+const User = require("../schema/UserSchema");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
+app.use(express.json());
+app.use(cookieParser());
+app.use(
+  cors({
     origin: "http://localhost:3000",
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.post("/register",async (req,res)=>{
-    
-    const {email,password} = await req.body;
+app.post("/register", async (req, res) => {
+  const { email, password } = await req.body;
 
-    if(!email || !password){
-        return res.status(404).json({message: "error"})
+  if (!email || !password) {
+    return res.status(404).json({ message: "error" });
+  }
+  await connect();
+  try {
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(404).json({ message: "User already exist" });
     }
-    await connect();
-    try {
-        const existingUser = await User.findOne({email});
-        if(existingUser){
-            return res.status(404).json({message: "User already exist"})
-        }
-        const schema = await User.create({
-            email,password
-        })
-        schema.save();
-        const token = jwt.sign({
-            data: email
-          }, 'abc', { expiresIn: '10h' });
-          res.cookie("token",token, {
-            
-                httpOnly: true, 
-                secure: false,  
-                sameSite: "lax", 
-          })
-        return res.status(200).json({message: "User saved"})
-    } catch (error) {
-        return res.status(500).json({message: "User not saved"})
-    }
-})
+    const schema = await User.create({
+      email,
+      password,
+    });
+    schema.save();
+    const token = jwt.sign(
+      {
+        data: email,
+      },
+      "abc",
+      { expiresIn: "10h" }
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+    return res.status(200).json({ message: "User saved", token });
+  } catch (error) {
+    return res.status(500).json({ message: "User not saved" });
+  }
+});
 
-module.exports = app
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const existingUser = await User.findOne({ email });
+    if (!existingUser)
+      return res.status(404).json({ message: "USER NOT FOUND" });
+    if (existingUser.password !== password)
+      return res.status(400).json({ message: "Incorrect credentials" });
+    const token = jwt.sign(
+      {
+        data: email,
+      },
+      "abc",
+      { expiresIn: "10h" }
+    );
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+    return res.status(200).json({ message: "Login successful" });
+  } catch (error) {
+    return res.status(500).json({ message: "Cannot login" });
+  }
+});
+
+module.exports = app;
